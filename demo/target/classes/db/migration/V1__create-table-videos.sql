2dca760cff5620ddbde100671da26b9b11944a0d
Gustavo Cadamuro <63973237+gu1334@users.noreply.github.com>
2024-09-28 14:40:11 -0300

    Delete demo directory

--- demo/target/classes/db/migration/V1__create-table-videos.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE banco (
-                       id BIGINT NOT NULL AUTO_INCREMENT,
-                       titulo VARCHAR(180) NOT NULL,
-                       descricao VARCHAR(180) NOT NULL,
-                       url VARCHAR(180) NOT NULL,
-                       PRIMARY KEY (id)
-);
